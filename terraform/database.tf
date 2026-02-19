2b16db29586a4ba3af45a1fd2d061d59c5613779
mercynwaka <nwakamercy@gmail.com>
2026-02-19 20:45:24 +0000

    updted app.tf

--- terraform/database.tf
+++ terraform/database.tf
@@ -80,7 +80,7 @@ resource "aws_db_instance" "catalog" {
   engine_version         = "8.0"
   instance_class         = "db.t3.micro"
   db_name                = "catalog"
-  password               = "BedrockPassword2026!"
+  
   parameter_group_name   = "default.mysql8.0"
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
